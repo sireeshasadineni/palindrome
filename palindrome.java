e056451a9b652681a53e6d1f0f62da6e79cffa04
sireeshasadineni <41733085+sireeshasadineni@users.noreply.github.com>
2018-08-01 15:35:04 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num1,num2,sum=0;
+        Scanner sc=new Scanner(System.in);  
+        num1= sc.nextInt(); 
+        num2=num1;
+        while(num2>0)
+        {
+            sum = (sum*10) + (num2%10);
+            num2/=10;
+        }      
+        if(num1==sum)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+	}
+}
